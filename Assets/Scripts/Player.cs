8a10557beddc920dacbd6de70b797b6fc3758f0e
aemc64 <aemc.64@gmail.com>
2023-02-04 11:59:11 -0300

    Added gamepad support

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _inputPlayerId = 0;
     [SerializeField] private float _speed = 20.0f;
     [SerializeField] private float _rotationSpeed = 1f;
 
     private Rigidbody _rb;
     private Camera _camera;
+    private Rewired.Player _inputPlayer;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _camera = Camera.main;
+        _inputPlayer = ReInput.players.GetPlayer(_inputPlayerId);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        var inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        var inputVector = new Vector3(_inputPlayer.GetAxisRaw("Horizontal"), 0f, _inputPlayer.GetAxisRaw("Vertical"));
 
         var movementDirection = _camera.transform.TransformDirection(inputVector);
         movementDirection.y = 0f;
@@ -27,10 +29,9 @@ public class Player : MonoBehaviour {
         
         _rb.velocity = movementDirection * _speed;
 
-        if (inputVector != Vector3.zero)
-        {
-            var targetRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
-        }
+        if (inputVector == Vector3.zero) return;
+        
+        var targetRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
     }
 }
